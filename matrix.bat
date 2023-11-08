0a6dc0f863c754c1a4805e87336c093a9d3fa1ff
Kali <116874178+Kali3159@users.noreply.github.com>
2023-11-08 11:38:48 +0100

    Initial commit

---
+++ matrix.bat
@@ -0,0 +1,13 @@
+@echo off
+color a
+title Animation Matrix by Batchapp // Correction Shoot
+:m
+echo          %random% %random% %random% %random% %random% %random%
+echo      %random%   %random%%random% %random%    %random%   %random% %random%
+echo %random%   %random%%random% %random%    %random%   %random% %random%
+echo  %random%   %random%%random% %random%    %random%   %random% %random%
+echo    %random%  %random%   %random% %random%           %random% %random%
+echo      %random%   %random%%random% %random%    %random%  %random% %random%
+echo %random%   %random%   %random% %random%    %random%     %random%     %random%
+echo  %random%   %random%   %random% %random%    %random%   %random% %random% 
+goto m
